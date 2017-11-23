e26a5ac764f77f142365aeee3baaf0a706ae6939
Keri Warr <keri@warr.ca>
2017-11-23 00:37:22 -0500

    Require specific ramda methods

---
+++ ramda.js
@@ -0,0 +1,11 @@
+module.exports = ({
+  compose: require('ramda').compose,
+  fromPairs: require('ramda').fromPairs,
+  map: require('ramda').map,
+  mergeAll: require('ramda').mergeAll,
+  pick: require('ramda').pick,
+  propOr: require('ramda').propOr,
+  toPairs: require('ramda').toPairs,
+  type: require('ramda').type,
+  values: require('ramda').values
+})
