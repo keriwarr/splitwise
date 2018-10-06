c0178130b65e01eaf5310b57a1d383d645b9f47c
Keri Warr <keri@faire.com>
2018-10-06 02:08:47 -0400

    Improve test coverage accuracy

---
+++ __tests__/splitwise.test.js
@@ -0,0 +1,31 @@
+module.exports = (function () {
+  'use strict'
+
+  const { OAuth2 } = require('oauth')
+  const Splitwise = require('../src')
+
+  jest.mock('oauth')
+
+  beforeEach(() => {
+    // Clear all instances and calls to constructor and all methods:
+    OAuth2.mockClear()
+  })
+
+  test('creates splitwise compatible parameters', () => {
+    // Show that mockClear() is working:
+    expect(OAuth2).not.toHaveBeenCalled()
+
+    Splitwise({
+      consumerKey: 'your key here',
+      consumerSecret: 'your secret here'
+    })
+    // Constructor should have been called again:
+    expect(OAuth2).toHaveBeenCalledTimes(1)
+
+    // mock.instances is available with automatic mocks:
+    const mockOAuthInstance = OAuth2.mock.instances[0]
+    const mockGetOAuthAccessToken = mockOAuthInstance.getOAuthAccessToken
+    // Equivalent to above check:
+    expect(mockGetOAuthAccessToken).toHaveBeenCalledTimes(1)
+  })
+})()
